9baaea8fcdb677318003c0cb01d1d028c535c904
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2024-10-02 18:00:12 +0300

    Add onboarding login screen with custom styled buttons and text.

--- lib/features/Auth/presentation/views/onboarding_login_signup.dart
+++ lib/features/Auth/presentation/views/onboarding_login_signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../config/router/routes.dart';
 import '../../../../core/utils/colors.dart';
 import '../../../../core/utils/string.dart';
 import '../widgets/custom_bottom.dart';
@@ -15,74 +16,89 @@ class OnboardingLoginSignup extends StatelessWidget {
       body: SafeArea(
           child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 180.h,
-              ),
-              Center(
-                child: Text(
-                  "meem",
-                  style: TextStyle(
-                    fontSize: 98.sp,
-                    fontFamily: StringManager.fontFamily,
-                    fontWeight: FontWeight.bold,
-                    color: ColorsManager.white,
+          padding:  EdgeInsets.symmetric(horizontal: 12.w),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 180.h,
+                ),
+                Center(
+                  child: Text(
+                    "meem",
+                    style: TextStyle(
+                      fontSize: 98.sp,
+                      fontFamily: StringManager.fontFamily,
+                      fontWeight: FontWeight.bold,
+                      color: ColorsManager.white,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 40.h,
-              ),
-              Center(
-                child: Text(
-                  "Shop Loud, Live Bold. Make\nEvery Purchase a Statement",
-                  style: TextStyle(
-                    fontSize: 22.sp,
-                    fontFamily: StringManager.fontFamily,
-                    fontWeight: FontWeight.normal,
-                    color: ColorsManager.white,
+                SizedBox(
+                  height: 60.h,
+                ),
+                Center(
+                  child: Text(
+                    "Shop Loud, Live Bold. Make\nEvery Purchase a Statement",
+                    style: TextStyle(
+                      fontSize: 22.sp,
+                      fontFamily: StringManager.fontFamily,
+                      fontWeight: FontWeight.normal,
+                      color: ColorsManager.white,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 100.h,
-              ),
-              CustomBorderBottom(
-                text: 'Login',
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50.h,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: ColorsManager.white,width: 1.5.sp),
-                      color: ColorsManager.mainPink,
-                      borderRadius: BorderRadius.circular(12.r)),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Center(
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                              fontSize: 22.sp,
-                              fontFamily: StringManager.fontFamily,
-                              fontWeight: FontWeight.w500,
-                              color: ColorsManager.white,
+                SizedBox(
+                  height: 128.h,
+                ),
+                CustomBorderBottom(
+                  text: 'Login',
+                  onPressed: () {
+                    Navigator.pushNamed(context, Routes.logIn);
+                  },
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                InkWell(
+                  hoverColor: Colors.white,
+                  splashColor: Colors.white,
+                  borderRadius: BorderRadius.circular(12.r),
+                  onTap: () {
+                    Navigator.pushNamed(context, Routes.Signup);
+                  },
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.r),
+                      border: Border.all(
+                        color: ColorsManager.white, // White border
+                        width: 1.3.sp,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w), // Padding for the button content
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                fontSize: 22.sp,
+                                fontFamily: StringManager.fontFamily,
+                                fontWeight: FontWeight.w500,
+                                color: ColorsManager.white,
+                              ),
                             ),
                           ),
-                        ),
-                      ]),
-                ),
-              )
-            ],
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       )),
